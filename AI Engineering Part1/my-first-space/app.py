4e59f9bcca57a458df46a4647535cd8ddec6d46e
gkamath777 <88.kamath@gmail.com>
2026-07-21 08:22:28 -0700

    Added two changes as per render requirement.

--- AI Engineering Part1/my-first-space/app.py
+++ AI Engineering Part1/my-first-space/app.py
@@ -1,9 +1,19 @@
+import os
 import gradio as gr
 
 
 def respond(message, history):
-    response = f"you said: {message}\
-        \n And I say I love learning AI engineering"
+    response = (
+        f"You said: {message}\n"
+        "And I say: I love learning AI engineering!"
+    )
     return response
 
-gr.ChatInterface(fn=respond).launch()
+
+demo = gr.ChatInterface(fn=respond)
+
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
